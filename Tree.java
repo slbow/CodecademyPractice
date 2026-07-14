d53cd8f8591cb20b8361a4b38174acdd5a1f8695
slbow <236501052+slbow@users.noreply.github.com>
2026-07-14 11:44:49 -0500

    Create Tree.java to print ASCII trees
    This Java program introduces the author and demonstrates ASCII art by printing small and large tree representations.

---
+++ Tree.java
@@ -0,0 +1,36 @@
+// Define your tree class in this file!
+public class Tree{
+  public static void main(String[] args){
+    /* 
+    This is a single file Java program. Purpose: To introduce myself and practice print statements by making a ASCII tree
+    */
+    System.out.println("Greetings, I am S, and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+
+    // Small tree print statements
+    System.out.println("     ");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+    System.out.println("~~~~~");
+
+    // Empty line to seperate trees
+    System.out.println("     ");
+
+    // Large tree print statements
+    System.out.println(" ^^^ ");
+    System.out.println(" ^^^ ");
+    System.out.println("^^^^^");
+    System.out.println("^^^^^");
+    System.out.println(" ^^^ ");
+    System.out.println(" ^^^ ");
+    System.out.println("  #  ");
+    System.out.println("  #  ");
+    System.out.println("  #  ");
+    System.out.println("~~~~~");
+
+
+  }
+}
