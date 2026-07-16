7d92cb72df9b15286308dad45663dca005dd8d64
slbow <236501052+slbow@users.noreply.github.com>
2026-07-16 17:22:12 -0500

    Create MadLibs.java to generate a mad lib story
    This Java program generates a mad libbed story using predefined variables for names, adjectives, verbs, nouns, a number, and a place.

---
+++ MadLibs.java
@@ -0,0 +1,47 @@
+public class MadLibs {
+  /*
+  This Java program generates a mad libbed story.
+  Author: Meow
+  Date: 7/16/2026
+  */
+  public static void main(String[] args){
+    // Protagonist name
+    String name1 = "Donald Trump";
+
+    // Adjectives for the story
+    String adjective1 = "Fat";
+    String adjective2 = "Red";
+    String adjective3 = "cat-like";
+
+    // Verb for the story, present tense
+    String verb1 = "jump";
+
+    // Nouns for the story
+    String noun1 = "the mall";
+    String noun2 = "Jim Kong June";
+    String noun3 = "Taco Bell";
+    String noun4 = "Prison";
+    String noun5 = "Shawshank Prison";
+    String noun6 = "Super Prison";
+
+    // Name for the second character, non - protagonist
+    String name2 = "Danny McBride";
+    
+    // Number required for the story
+    int number = -9999;
+
+    // Place variable required for the story
+    String place1 = "a Quasar";
+
+    
+
+
+
+    
+    
+    //The template for the story
+    String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+
+    System.out.println(story);
+  }       
+}
