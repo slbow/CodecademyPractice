d83d60067e2e26be0753e680d5a143555dcaf299
slbow <236501052+slbow@users.noreply.github.com>
2026-07-21 13:01:53 -0500

    Create block_letters.py to display 'Jake' and 'C'

---
+++ block_letters.py
@@ -0,0 +1,10 @@
+# My name is Jake the cat
+# Fun fact: I am a cat
+
+print("     JJJJJ   CCCCC")
+print("       J    C")
+print("       J    C")
+print("       J    C")
+print("       J    C")
+print("   J   J    C")
+print("    JJJ      CCCCC")
